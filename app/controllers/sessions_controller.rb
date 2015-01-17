aa88eb0dcbd067369da25bde352e16c498484d15
Scott Yu <scott.tj.yu@gmail.com>
2015-01-17 10:36:30 +0800

    authentication fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
-    cookies[:user_id]
+    cookies[:user_id] = user.id
     flash[:success] = "Hello, #{user.name}!"
-    redirect_to root_url 
-  end
-  
-  def destroy
-    cookies.delete(:user_id)
-    flash[:success] = "See ya!"
     redirect_to root_url
   end
-
-  def failure
-    reder text: "You've tried to authenticate via #{params[:strategy]}, but the following error occurred: #{params[:message]}, please try again."
+ 
+  def destroy
+    cookies.delete(:user_id)
+    flash[:success] = "See you!"
+    redirect_to root_url
+  end
+   
+  def auth_fail
+    render text: "You've tried to authenticate via #{params[:strategy]}, but the following error occurred: #{params[:message]}", status: 500
   end
 end
