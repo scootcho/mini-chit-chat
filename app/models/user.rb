fb90bc3fe9bf5ef89343a4bf50e6c572a3d723ba
Scott Yu <scott.tj.yu@gmail.com>
2015-01-19 10:08:58 +0800

    user model update to store profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ActiveRecord::Base
-  has_many :comments, dependent: :delete_all
-
   class << self
     def from_omniauth(auth)
       provider = auth.provider
       uid = auth.uid
-      info = auth.info
+      info = auth.info.symbolize_keys!
       user = User.find_or_initialize_by(uid: uid, provider: provider)
       user.name = info.name
-      user.avatar_url
+      user.avatar_url = info.image
       user.profile_url = info.urls.send(provider.capitalize.to_sym)
       user.save!
       user
