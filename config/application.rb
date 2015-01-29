8f864cf0aab7e02025e41b7c841f6b78e7817a54
Scott Yu <scott.tj.yu@gmail.com>
2015-01-29 16:43:07 +0800

    fixed faye application.rb config

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,7 @@ module MiniChat
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.middleware.delete Rack::Lock
-    config.middleware.use FayeRails::Middleware, extensions: [CsrfProtection.new], mount: '/faye', :timeout => 25
-
+  config.middleware.delete Rack::Lock
+  config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
   end
 end
